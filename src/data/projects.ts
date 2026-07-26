750912c0ff61d4781d7d8460fd41720d89d0f2a3
Diego Salcedo <diegoandres2020.1003@gmail.com>
2026-07-26 15:06:40 -0500

    z

---
+++ src/data/projects.ts
@@ -0,0 +1,40 @@
+import hero from "../assets/hero-bg.png";
+
+export const projects = [
+    {
+        title: "Inventory Management System",
+        description: "A web application for managin inventory, tracking stock levels, and generating reports.",
+        tags: ["React", "Node.js", "Laravel", "MySQL", "Tailwind CSS"],
+        image: hero,
+    },
+    {
+        title: "Particle Background",
+        description: "A visually appealing particle background effect for websites, enhancing user experience.",
+        tags: ["TypeScript", "HTML Canvas", "CSS"],
+        image: hero,
+    },
+    {
+        title: "Vehicle Inspection Management System",
+        description: "A web application for managing vehicle inspections, scheduling appointments, and generating inspection reports.",
+        tags: ["React", "Node.js", "Laravel", "MySQL", "Tailwind CSS"],
+        image: hero,
+    },
+    {
+        title: "Inventory Management System",
+        description: "A web application for managin inventory, tracking stock levels, and generating reports.",
+        tags: ["React", "Node.js", "Laravel", "MySQL", "Tailwind CSS"],
+        image: hero,
+    },
+    {
+        title: "Particle Background",
+        description: "A visually appealing particle background effect for websites, enhancing user experience.",
+        tags: ["TypeScript", "HTML Canvas", "CSS"],
+        image: hero,
+    },
+    {
+        title: "Vehicle Inspection Management System",
+        description: "A web application for managing vehicle inspections, scheduling appointments, and generating inspection reports.",
+        tags: ["React", "Node.js", "Laravel", "MySQL", "Tailwind CSS"],
+        image: hero,
+    }
+]
