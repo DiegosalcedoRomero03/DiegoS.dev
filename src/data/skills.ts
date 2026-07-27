45a90af48c63278b2af2dceb88298e2019413ff8
Diego Salcedo <diegoandres2020.1003@gmail.com>
2026-07-26 20:00:19 -0500

    He aplicado cambios en el main.astro del proyecto, header, hero, etc. Implementación de un diseño mas limpio al proyecto, una paleta de colores mejor definida, etc.

---
+++ src/data/skills.ts
@@ -0,0 +1,64 @@
+import php from "../assets/icons/php_dark.Cp36W25N_Z1RERN9.svg"
+import mysql from "../assets/icons/mysql.BoTyT-ZX_ZVA9JU.svg"
+import html from "../assets/icons/html.Cr996ATo_2hqtMw.svg"
+import css from "../assets/icons/css.BK6-i4DX_15MpSj.svg"
+import typescript from "../assets/icons/typescript.S--6xCZS_22wrNy.svg"
+import mongodb from "../assets/icons/mongodb.DQSGo-qR_Z11fdJ5.svg"
+import astro from "../assets/icons/astro_dark.BkH7Jeoz_2jIeQt.svg"
+import git from "../assets/icons/git-svgrepo-com.svg"
+import tailwind from "../assets/icons/tailwindcss.Cw5b2glz_Z112Ilu.svg"
+import docker from "../assets/icons/docker.svg"
+
+
+export const skills = [
+    {
+        name: "PHP",
+        icon: php,
+        description: "php icon"
+    },
+    {
+        name: "MySQL",
+        icon: mysql,
+        description: "mysql icon"
+    },
+    {
+        name: "HTML",
+        icon: html,
+        description: "html icon"
+    },
+    {
+        name: "CSS",
+        icon: css,
+        description: "css icon"
+    },
+    {
+        name: "TypeScript",
+        icon: typescript,
+        description: "typescript icon"
+    },
+    {
+        name: "MongoDB",
+        icon: mongodb,
+        description: "mongodb icon"
+    },
+    {
+        name: "Astro",
+        icon: astro,
+        description: "astro icon"
+    },
+    {
+        name: "Git",
+        icon: git,
+        description: "git icon"
+    },
+    {
+        name: "Tailwind CSS",
+        icon: tailwind,
+        description: "tailwindcss icon"
+    },
+    {
+        name: "Docker",
+        icon: docker,
+        description: "docker icon"
+    },
+]
